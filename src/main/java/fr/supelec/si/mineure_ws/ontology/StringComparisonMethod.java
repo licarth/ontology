434ad355d088036d0cb979ed7ee0df3863598463
Thomas CARLI <thomascarli@gmail.com>
2014-02-26 16:07:06 +0100

    _

--- src/main/java/fr/supelec/si/mineure_ws/ontology/StringComparisonMethod.java
+++ src/main/java/fr/supelec/si/mineure_ws/ontology/StringComparisonMethod.java
@@ -2,9 +2,12 @@ package fr.supelec.si.mineure_ws.ontology;
 
 import fr.supelec.si.mineure_ws.ontology.distance.Hamming;
 import fr.supelec.si.mineure_ws.ontology.distance.StringDistanceComparator;
+import fr.supelec.si.mineure_ws.ontology.distance.SubString;
 
 public enum StringComparisonMethod {
-	HAMMING(new Hamming());
+	HAMMING(new Hamming()),
+	SUBSTRING(new SubString()),
+	;
 	
 	private StringDistanceComparator comparisonStrategy;
 	
