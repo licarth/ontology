18ab1f72d5467443aad8b89d6cc9447e16ff9476
tholz <tholz@172.29.11.137>
2014-03-05 15:05:06 +0100

    h

--- src/main/java/fr/supelec/si/mineure_ws/ontology/distance/Synonyms.java
+++ src/main/java/fr/supelec/si/mineure_ws/ontology/distance/Synonyms.java
@@ -18,20 +18,7 @@ public class Synonyms extends LangBased {
 	public double getSimilarity(String word1, String word2) {
 		String nWord1 = normalizeWord(word1);
 		String nWord2 = normalizeWord(word2);
-<<<<<<< HEAD
-=======
-		
-		RiWordnet wordnet = new RiWordnet();
-		
-		String pos1 = wordnet.getBestPos(nWord1);
-		if (pos1 == null) return 0.0;
-		String pos2 = wordnet.getBestPos(nWord2);
-		if (pos2 == null) return 0.0;
-		
-		String[] synonyms1 = wordnet.getAllSynonyms(nWord1, pos1);
->>>>>>> branch 'master' of git@github.com:licarth/ontology.git
 
-<<<<<<< HEAD
 		String[] pos = wordnet.getPos(nWord1);
 
 		if(pos!=null){
@@ -44,18 +31,7 @@ public class Synonyms extends LangBased {
 						allSynonyms.add(allSynsets[i]);
 					}
 				}
-=======
-		for(int i = 0; i<synonyms1.length; i++){
-			if(synonyms1[i].equals(nWord2))
-				return 1.0;
-		}
-		
-		String[] synonyms2 = wordnet.getAllSynonyms(nWord2, pos2);
-		for(int i = 0; i<synonyms2.length; i++){
-			if(synonyms2[i].equals(nWord1)){
-				System.out.println("R�f�rencement non bijectif.");
-				return 1.0;
->>>>>>> branch 'master' of git@github.com:licarth/ontology.git
+
 			}
 
 
